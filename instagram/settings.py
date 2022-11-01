1a7b7cebfa2f5fe2d45d571733b8d76a3f1205bc
vinayKumarreddy-mangalampenta <mangalapentavinaykumarreddy30@gmail.com>
2022-11-01 15:09:42 +0530

    added whitenoise

--- instagram/settings.py
+++ instagram/settings.py
@@ -85,6 +85,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -187,6 +188,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 if os.cwd() == 'app/':
     DEBUG = False
